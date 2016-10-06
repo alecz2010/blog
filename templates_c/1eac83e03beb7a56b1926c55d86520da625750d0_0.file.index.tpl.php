a85a74986690b03ab1c439db309dad59e551d426
alex.ratiu <contact@ratiuraul.ro>
2016-10-06 17:50:52 +0300

    initial commit  > smarty

---
+++ templates_c/1eac83e03beb7a56b1926c55d86520da625750d0_0.file.index.tpl.php
@@ -0,0 +1,42 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-10-06 16:15:41
+  from "C:\xampp\htdocs\blog\templates\index.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_57f65c8d004336_72161407',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '1eac83e03beb7a56b1926c55d86520da625750d0' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\blog\\templates\\index.tpl',
+      1 => 1475763332,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_57f65c8d004336_72161407 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<html>
+<head>
+    <title>Info</title>
+</head>
+<body>
+
+<pre>
+User Information:
+
+Name: <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+
+    Address: <?php echo $_smarty_tpl->tpl_vars['address']->value;?>
+
+</pre>
+
+</body>
+</html><?php }
+}
